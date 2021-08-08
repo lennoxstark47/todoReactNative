a3ddcd2e797ec5b42785c06c779919d7945228e3
Stark 47 <twisamhaldar@gmail.com>
2021-08-08 23:38:55 +0530

    1st commit

--- App.js
+++ App.js
@@ -1,21 +1,85 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import {
+	KeyboardAvoidingView,
+	Platform,
+	StyleSheet,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	View,
+} from 'react-native';
+import Task from './components/Task';
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={styles.tasksWrapper}>
+				<Text style={styles.sectionTitle}>
+					Today's tasks
+				</Text>
+				<View style={styles.items}>
+					<Task name={'This is task 1'} />
+					<Task name={'This is task 2'} />
+					<Task name={'This is task 3'} />
+					<Task name={'This is task 4'} />
+				</View>
+			</View>
+			<StatusBar style='auto' />
+			<KeyboardAvoidingView
+				behavior={
+					Platform.OS === 'ios'
+						? 'padding'
+						: 'height'
+				}
+				style={styles.writeTaskWrapper}>
+				<TextInput
+					style={styles.input}
+					placeholder={'Write a task'}
+				/>
+				<TouchableOpacity>
+					<View style={styles.addWrapper}>
+						<Text style={styles.addText}>+</Text>
+					</View>
+				</TouchableOpacity>
+			</KeyboardAvoidingView>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#E8EAED',
+	},
+
+	tasksWrapper: {
+		paddingTop: 80,
+		paddingHorizontal: 20,
+	},
+	sectionTitle: {
+		fontSize: 24,
+		fontWeight: 'bold',
+	},
+	items: {
+		marginTop: 30,
+	},
+	writeTaskWrapper: {
+		position: 'absolute',
+		bottom: 60,
+		width: '100%',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+	input: {
+		paddingVertical: 15,
+		paddingHorizontal: 15,
+		width: 250,
+		backgroundColor: '#FFF',
+		borderRadius: 60,
+		borderColor: '#C0C0C0',
+		borderWidth: 1,
+	},
+	addWrapper: {},
+	addText: {},
 });
